1e25f2f64676f65c569a1fefd8cd56982a43c80f
Pavel <npvel@mail.ru>
2024-12-07 14:56:35 +0100

    Ready for deployment

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FrenchDialogueExercise from './components/excersise1_1'
+
+function App() {
+  return (
+    <div>
+      <FrenchDialogueExercise />
+    </div>
+  )
+}
+
+export default App
